d26ed908e298b9457078feddf76abaa06ee60722
Hyohwak Lee <crewe1746@naver.com>
2022-10-01 15:59:12 +0900

    Run postgres on docker and connect it in webapp

--- webapp/cmd/web/main.go
+++ webapp/cmd/web/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 
@@ -9,11 +11,24 @@ import (
 
 type application struct {
 	Session *scs.SessionManager
+	DB      *sql.DB
+	DSN     string
 }
 
 func main() {
 	// set up an app config
 	app := application{}
+
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Postgres Connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
+
 	// get a session manager
 	app.Session = getSession()
 
