bad23dee77819e1d54dc5de9960329958432bc68
Hyohwak Lee <crewe1746@naver.com>
2022-10-02 20:17:38 +0900

    Write test for ResetUserPassword and InsertUserImage

--- webapp/pkg/repository/dbrepo/users_postgres_test.go
+++ webapp/pkg/repository/dbrepo/users_postgres_test.go
@@ -1,3 +1,5 @@
+//go:build integration
+
 package dbrepo
 
 import (
@@ -228,5 +230,46 @@ func TestPostgresDBRepoDeleteUser(t *testing.T) {
 	_, err = testRepo.GetUser(2)
 	if err == nil {
 		t.Error("retrieved user id 2, who should have been deleted")
+	}
+}
+
+func TestPostgresDBRepoResetPassword(t *testing.T) {
+	err := testRepo.ResetPassword(1, "password")
+	if err != nil {
+		t.Error("error reseting user's password")
+	}
+
+	user, _ := testRepo.GetUser(1)
+	matches, err := user.PasswordMatches("password")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !matches {
+		t.Error("password should match 'password' but does not")
+	}
+}
+
+func TestPostgresDBRepoInsertUserImage(t *testing.T) {
+	image := data.UserImage{
+		UserID:    1,
+		FileName:  "test.jpg",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	newId, err := testRepo.InsertUserImage(image)
+	if err != nil {
+		t.Error("inserting user image failed:", err)
+	}
+
+	if newId != 1 {
+		t.Error("got wrong id for image; it should be 1 but got", newId)
+	}
+
+	image.UserID = 100
+	_, err = testRepo.InsertUserImage(image)
+	if err == nil {
+		t.Error("inserted a user image with non-existent user id")
 	}
 }
