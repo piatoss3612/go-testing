33afce7289a13613027b636e0d839d17ac22ee14
Hyohwak Lee <crewe1746@naver.com>
2022-10-02 12:53:15 +0900

    Setup databse integration test using docker

---
+++ webapp/pkg/repository/dbrepo/users_postgres_test.go
@@ -0,0 +1,115 @@
+package dbrepo
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "postgres"
+	dbName   = "users_test"
+	port     = "5435"
+	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
+)
+
+var (
+	resource *dockertest.Resource
+	pool     *dockertest.Pool
+	testDB   *sql.DB
+)
+
+func TestMain(m *testing.M) {
+	// connect to docker; fail if docker not running
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("could not connect to docker; is it running? %s", err)
+	}
+
+	pool = p
+
+	// set up docker options, specifying image and so forth
+	opts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "14.5",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: port},
+			},
+		},
+	}
+
+	// get a resource (docker image)
+	resource, err = pool.RunWithOptions(&opts)
+	if err != nil {
+		log.Fatalf("could not start resource: %s", err)
+	}
+
+	// start image and wait until container is ready
+	if err := pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			log.Println("error:", err)
+			return err
+		}
+
+		return testDB.Ping()
+	}); err != nil {
+		pool.Purge(resource)
+		log.Fatalf("could not connect to database: %s", err)
+	}
+
+	// populate database with empty tables
+	if err = createTables(); err != nil {
+		log.Fatalf("error creating tables: %s", err)
+	}
+
+	// run tests
+	code := m.Run()
+
+	// clean up
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
+}
+
+func createTables() error {
+	tableSQL, err := os.ReadFile("./testdata/users.sql")
+	if err != nil {
+		return err
+	}
+
+	_, err = testDB.Exec(string(tableSQL))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Test_pingDB(t *testing.T) {
+	err := testDB.Ping()
+	if err != nil {
+		t.Error("cannot ping database")
+	}
+}
