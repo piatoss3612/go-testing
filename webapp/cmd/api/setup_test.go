3bdc7cf0354000c9b3b78da08788d96ddad94497
Hyohwak Lee <crewe1746@naver.com>
2022-10-04 19:01:20 +0900

    Write test for generate/validate tokens and check test coverage

--- webapp/cmd/api/setup_test.go
+++ webapp/cmd/api/setup_test.go
@@ -6,7 +6,10 @@ import (
 	"webapp/pkg/repository/dbrepo"
 )
 
-var app application
+var (
+	app          application
+	expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiYXVkIjoiZXhhbXBsZS5jb20iLCJleHAiOjE2NjQ1MTM2NDQsImlzcyI6ImV4YW1wbGUuY29tIiwibmFtZSI6IkpvaG4gRG9lIiwic3ViIjoiMSJ9.VjODp43PPUlT-nVDSrPNTaXhkhlNuyz9KhJaHEukFMs"
+)
 
 func TestMain(m *testing.M) {
 	app.DB = &dbrepo.TestDBRepo{}
