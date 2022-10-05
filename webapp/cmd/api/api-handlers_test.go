25abf425df3db651a6dc21d7cc7353907a292283
Hyohwak Lee <crewe1746@naver.com>
2022-10-05 22:07:38 +0900

    Implement refresh handler and test it

--- webapp/cmd/api/api-handlers_test.go
+++ webapp/cmd/api/api-handlers_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
+	"time"
+	"webapp/pkg/data"
 )
 
 func Test_app_authenticate(t *testing.T) {
@@ -21,8 +23,7 @@ func Test_app_authenticate(t *testing.T) {
 	}
 
 	for _, e := range tests {
-		var reader io.Reader
-		reader = strings.NewReader(e.requestBody)
+		reader := strings.NewReader(e.requestBody)
 		req, _ := http.NewRequest("POST", "/auth", reader)
 		rr := httptest.NewRecorder()
 		handler := http.HandlerFunc(app.authenticate)
@@ -32,5 +33,56 @@ func Test_app_authenticate(t *testing.T) {
 		if rr.Code != e.expectedStatusCode {
 			t.Errorf("%s: returned wrong status code; expected %d but got %d", e.name, e.expectedStatusCode, rr.Code)
 		}
+	}
+}
+
+func Test_app_refresh(t *testing.T) {
+	tests := []struct {
+		name               string
+		token              string
+		expectedStatusCode int
+		resetRefreshTime   bool
+	}{
+		{"valid", "", http.StatusOK, true},
+		{"valid but not yet ready to expire", "", http.StatusTooEarly, false},
+		{"expired token", expiredToken, http.StatusBadRequest, false},
+	}
+
+	testUser := data.User{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+		Email:     "admin@example.com",
+	}
+
+	oldRefreshTime := refreshTokenExpiry
+
+	for _, e := range tests {
+		var tkn string
+		if e.token == "" {
+			if e.resetRefreshTime {
+				refreshTokenExpiry = time.Second * 1
+			}
+			tokens, _ := app.generateTokenPair(&testUser)
+			tkn = tokens.RefreshToken
+		} else {
+			tkn = e.token
+		}
+
+		postedData := url.Values{
+			"refresh_token": {tkn},
+		}
+
+		req, _ := http.NewRequest("POST", "/refresh-token", strings.NewReader(postedData.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(app.refresh)
+		handler.ServeHTTP(rr, req)
+
+		if rr.Code != e.expectedStatusCode {
+			t.Errorf("%s: expected status of %d but got %d", e.name, e.expectedStatusCode, rr.Code)
+		}
+
+		refreshTokenExpiry = oldRefreshTime
 	}
 }
