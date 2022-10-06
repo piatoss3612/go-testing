812a8646bebadd278c0fe64c5ccb5457c4dd9cf7
Hyohwak Lee <crewe1746@naver.com>
2022-10-06 16:51:56 +0900

    Interact with get user id 1 and logout button

--- webapp/cmd/api/api-routes.go
+++ webapp/cmd/api/api-routes.go
@@ -19,7 +19,7 @@ func (app *application) routes() http.Handler {
 	mux.Route("/web", func(mux chi.Router) {
 		mux.Post("/auth", app.authenticate)
 		mux.Get("/refresh-token", app.refreshUsingCookie)
-		// /logout
+		mux.Get("/logout", app.deleteRefreshCookie)
 	})
 
 	// authentication routes - auth handler, refresh
